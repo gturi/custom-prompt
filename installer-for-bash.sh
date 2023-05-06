23917adf187f365d245fa66ab41ba466474a7b0d
gventurini <giacomo.venturini3@gmail.com>
2023-05-06 12:34:33 +0200

    remove gitmodule and update install scripts

--- installer-for-bash.sh
+++ installer-for-bash.sh
@@ -3,6 +3,9 @@
 # gets the absolute path to the directory containing this script
 DIR="$(dirname "$(readlink -f "$0")")"
 
+cd "$DIR" || exit
+
+git clone https://github.com/gturi/get-git-branch-name.git\
 /bin/bash "$DIR/get-git-branch-name/installer-for-bash.sh"
 
 SCRIPT="$DIR/custom-prompt-bash.sh"
