b98b4ed7ed30f847a15d4b59fc94def067e1cf7a
gventurini <giacomo.venturini3@gmail.com>
2023-05-07 11:13:49 +0200

    fix update script to clone repo if not existing

--- update.sh
+++ update.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
+export SHELLOPTS
+
+cd "$(dirname "$0")"
+
 git pull
-cd get-git-branch-name || exit
+
+[ ! -d "get-git-branch-name" ] && git clone https://github.com/gturi/get-git-branch-name.git
+
+cd get-git-branch-name
 git pull
