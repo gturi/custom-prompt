0f53e1fea10ab5fe38f265243fdd6d9bcee82925
gventurini <giacomo.venturini3@gmail.com>
2025-04-09 23:56:03 +0200

    add prompt to abort the setup of .bashrc and .zshrc

--- installer-for-bash.sh
+++ installer-for-bash.sh
@@ -7,6 +7,16 @@ cd "$DIR" || exit
 
 ./setup.sh
 
+read -p "custom-prompt: do you wish to add the utility to your .bashrc? [y/n]" -n 1 -r
+echo # moves to a new line
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  log "custom-prompt: .bashrc setup aborted"
+  # handle exits from shell or function but don't exit interactive shell
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 0 || return 0
+fi
+
+
 /bin/bash "$DIR/get-git-branch-name/installer-for-bash.sh"
 
 SCRIPT="$DIR/custom-prompt-bash.sh"
