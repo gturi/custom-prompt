cd2215e5059b6bb52a8ae63fe8a2150ca12131ea
gturi <giacomo.venturini3@gmail.com>
2025-05-13 08:38:21 +0200

    add python virtual env info to prompt when in virtual environment

--- custom-prompt-zsh.sh
+++ custom-prompt-zsh.sh
@@ -1,18 +1,24 @@
 #!/bin/zsh
 
+function get_virtualenv_info {
+  [ $VIRTUAL_ENV ] && echo "("$(basename $VIRTUAL_ENV)") "
+}
+
 function get_prompt {
   local white='%F{white}%b'
   local green_bold='%F{green}%B'
   local blue_bold='%F{blue}%B'
   local red_bold='%F{red}%B'
 
+  local virtual_env_info="$(get_virtualenv_info)"
   local user_and_hostname='%n@%m'
   local current_dir='%1~'
   local branch_name="$(get_git_branch_name)"
   local dollar_symbol='%#'
 
   setopt PROMPT_SUBST
-  printf '%s%s%s%s%s%s ' \
+  printf '%s%s%s%s%s%s%s ' \
+        "${virtual_env_info}" \
         "${white}[" \
         "${green_bold}${user_and_hostname}" \
         "${white}]" \
